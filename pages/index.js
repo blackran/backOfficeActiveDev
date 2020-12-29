3da1245c84686d77d91fee666a32d53930d00a11
blackran <andrianantenainarasolondraibe@gmail.com>
2020-12-29 10:53:19 +0300

    test4 configure bashurl

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home () {
             <main>
                 <div className='Home'>
                     <div className='center'>
-                        <Link href={ prefix + 'Login'}>
+                        <Link href={ '/Login'}>
                             Login
                         </Link>
                         Home page
